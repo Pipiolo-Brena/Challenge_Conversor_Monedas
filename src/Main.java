3599014ab935a48fb36d67b30f05def2e81ef2f3
Pipiolo-Brena <bjvictor18@gmail.com>
2025-08-18 22:21:45 -0600

    Refactorizando en funciones para poder hacer las conversiones de cualquier moneda

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -9,32 +10,51 @@ import com.google.gson.JsonParser;
 public class Main {
     public static void main(String[] args) {
         try {
-            HttpClient client = HttpClient.newHttpClient();
+            Scanner scanner= new Scanner(System.in);
 
-            String apiKey="78058626b6f5a16b34e84816";
-            String url="https://v6.exchangerate-api.com/v6/"+apiKey+"/latest/USD";
+            System.out.println("Ingrese su moneda base:");
+            String moneda=scanner.nextLine().toUpperCase();
 
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .build();
+            System.out.println("Ingrese su moneda a convertir:");
+            String convertir=scanner.nextLine().toUpperCase();
 
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JsonObject conversiones=tasas(moneda);
 
-            JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
-
-            String codigoMoneda = jsonResponse.get("base_code").getAsString();
-            JsonObject conversiones = jsonResponse.getAsJsonObject("conversion_rates");
-
-            String[] filtro={"ARS", "BOB", "BRL"};
-
-            System.out.println("Tasas de cambio monedas filtradas:");
-            for (String moneda : filtro) {
-                double valor = conversiones.get(moneda).getAsDouble();
-                System.out.println("1"+codigoMoneda+"->"+ moneda + ": " + valor);
+            if (!conversiones.has(convertir)) {
+                System.out.println("Moneda no disponible.");
+                return;
             }
+            double tasaConversion=conversiones.get(convertir).getAsDouble();
+
+            System.out.println("Ingrese su cantidad a convertir");
+            double cantidad=scanner.nextDouble();
+
+
+            System.out.println(cantidad + " " + moneda + " = " + covertirCantidad(cantidad,tasaConversion) + " " + convertir);
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static double covertirCantidad(double dinero,double tasa){
+        return dinero*tasa;
+    }
+
+    public static JsonObject tasas(String base)throws Exception{
+        HttpClient client = HttpClient.newHttpClient();
+
+        String apiKey="78058626b6f5a16b34e84816";
+        String url="https://v6.exchangerate-api.com/v6/"+apiKey+"/latest/"+base;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
+
+        return jsonResponse.getAsJsonObject("conversion_rates");
     }
 }
